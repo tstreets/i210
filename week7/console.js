a5ea7347625a6ead920fab7299db3611044f0997
Ty Streets <tstreets@iu.edu>
2024-10-08 11:35:55 -0400

    Upload week 7

---
+++ week7/console.js
@@ -0,0 +1,42 @@
+console.log("hi");
+
+let myAge = 52;
+
+myAge = 53;
+
+console.log(myAge);
+
+let myDog = {
+  hairColor: "green",
+  length: "10ft 3in",
+  teethCount: 274,
+  attribute: ["cuddly", "soft"],
+  name: "Shortridge High School",
+  collarColor: "blue",
+  updateToDateShots: true,
+  hasEatenLessThan5People: true,
+};
+
+console.log(myDog);
+
+console.log("Dog Name", myDog.name);
+console.log("Dog Friends:", myDog.friends);
+
+/* 
+    Create variables for the following:
+    - your name
+    - your favorite number
+    - whether you came to class today
+    - a monster and describe it's scary features (at least 3)
+*/
+
+let myName = "Ty the Mouse Guy";
+const favNum = 27;
+let imHere = true;
+const monster = {
+  hasTerribleTeeth: true,
+  numberOfTerribleEyes: 2,
+  colorOfTongue: "black",
+  colorOfWarts: "purple",
+  animalsEaten: ["cat", "dog", "mouse", "tiger"],
+};
