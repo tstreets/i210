2f3dce536e1c8f05aba072076123124a9febeee0
Ty Streets <tstreets@iu.edu>
2024-11-14 09:17:20 -0500

    Getting data from firebase

--- week12/connectfirebase.js
+++ week12/connectfirebase.js
@@ -1,6 +1,25 @@
 import { app } from "./firebase.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
+import {
+  getFirestore,
+  collection,
+  getDocs,
+} from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
+
+const booksListRef = document.getElementById("booksList");
 
 const db = getFirestore(app);
 
-console.log(db);
+async function getBooks() {
+  const booksCollection = collection(db, "books");
+  const booksDocs = await getDocs(booksCollection);
+
+  booksListRef.innerHTML = "";
+  for (let i = 0; i < booksDocs.docs.length; i++) {
+    const book = booksDocs.docs[i];
+    const bookData = book.data();
+
+    booksListRef.innerHTML += `<li>${bookData.title} (${bookData.yearPublished})</li>`;
+  }
+}
+
+getBooks();
